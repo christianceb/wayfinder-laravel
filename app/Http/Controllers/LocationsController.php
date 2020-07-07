c2e6e648b657753de69e442eea1182b9b2fc2f4e
Lanang Wisnugiri <lanangwisnugiri@gmail.com>
2020-07-07 19:23:58 +0800

    features/lanang115/30: fixed indentations

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -7,100 +7,100 @@ use Illuminate\Http\Request;
 
 class LocationsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        // show all items inside Locations Table
-        $allLocations = Locations::all();
-        return view('locations.index', [
-            'Locations' => $allLocations
-        ]);
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		// show all items inside Locations Table
+		$allLocations = Locations::all();
+		return view('locations.index', [
+			'Locations' => $allLocations
+		]);
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-        return view('locations.create');
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		//
+		return view('locations.create');
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        // validate the input for subject and description
-        request()->validate(
-            [
-                'locationsName' => ['required', 'max:50'],
-                'locationsType' => 'required',
-            ]
-        );
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		// validate the input for subject and description
+		request()->validate(
+			[
+				'locationsName' => ['required', 'max:50'],
+				'locationsType' => 'required',
+			]
+		);
 
-        $locations = new Locations();
-        $locations->name = $request->locationsName;
-        $locations->type = $request->locationsType;
-        $locations->save();
+		$locations = new Locations();
+		$locations->name = $request->locationsName;
+		$locations->type = $request->locationsType;
+		$locations->save();
 
-        return redirect('/locations')->with('success','Location created successfully.');
-    }
+		return redirect('/locations')->with('success', 'Location created successfully.');
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-        return view('locations.show');
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		//
+		return view('locations.show');
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-        return view('locations.update');
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		//
+		return view('locations.update');
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		//
 
-    }
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 }
--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -1,56 +1,56 @@
 @extends('layouts.app')
 
 @section('title')
-    Locations: Browse
+Locations: Browse
 @endsection
 
 @section('content')
-    <div class="container">
-        <h1 class="text-center">Browse Locations</h1>
-        <div class="mt-3 mb-3">
-            <a href="{{url("/locations/create")}}" class="btn btn-success">
-                Create
-            </a>
-        </div>
+<div class="container">
+	<h1 class="text-center">Browse Locations</h1>
+	<div class="mt-3 mb-3">
+		<a href="{{url("/locations/create")}}" class="btn btn-success">
+			Create
+		</a>
+	</div>
 
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Name</th>
-                <th scope="col">Type</th>
-                <th>Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($Locations as $location)
-                <tr>
-                    <td>{{$location->id}}</td>
-                    <td>{{$location->name}}</td>
-                    <td>
-                        {{App\Locations::getType($location->type)}}
-                    </td>
-                    <td>
-                        <form action="/feedbackSubjects/{{$location->id}}" method="post">
-                            @csrf
-                            @method('delete')
+	<table class="table">
+		<thead>
+			<tr>
+				<th scope="col">Id</th>
+				<th scope="col">Name</th>
+				<th scope="col">Type</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($Locations as $location)
+			<tr>
+				<td>{{$location->id}}</td>
+				<td>{{$location->name}}</td>
+				<td>
+					{{App\Locations::getType($location->type)}}
+				</td>
+				<td>
+					<form action="/feedbackSubjects/{{$location->id}}" method="post">
+						@csrf
+						@method('delete')
 
-                            <a href="{{url("/locations/{$location->id}")}}" class="btn btn-info">
-                                Show
-                            </a>
+						<a href="{{url("/locations/{$location->id}")}}" class="btn btn-info">
+							Show
+						</a>
 
-                            <a href="{{url("/locations/{$location->id}/edit")}}" class="btn btn-warning">
-                                Edit
-                            </a>
+						<a href="{{url("/locations/{$location->id}/edit")}}" class="btn btn-warning">
+							Edit
+						</a>
 
-                            <button type="submit" class="btn btn-danger">
-                                Delete
-                            </button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </div>
+						<button type="submit" class="btn btn-danger">
+							Delete
+						</button>
+					</form>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</div>
 @endsection
