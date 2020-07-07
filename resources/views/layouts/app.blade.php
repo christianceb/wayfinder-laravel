b95b067b116904f845510aaf6dd29731b834dbb8
Christian Ponce <55345116+christianconcepton@users.noreply.github.com>
2020-07-07 15:27:32 +0800

    Initial Laravel stuff: - Updated gitignore to exclude fonts - Added bootstrap, jquery and fontawesome. All should load locally

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
   </head>
   <body>
     @yield('content')
