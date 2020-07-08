fdb7d201a64a928d8fd0aa02329e2feba8902773
Christian Ponce <55345116+christianconcepton@users.noreply.github.com>
2020-07-09 00:00:29 +0800

    Completed base implementable BREAD components of Uploads collection

---
+++ resources/views/uploads/show.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('title')
+  Uploads: View
+@endsection
+
+@section('content')
+  <div class="container">
+    <h1>Uploads: View</h1>
+
+    <a class="btn btn-success" href="{{ route('uploads.index') }}" role="button">Back to Uploads</a>
+
+    <div class="row">
+      <div class="col-8">
+        <img src="{{$upload->url}}" alt="{{$upload->title}}" />
+      </div>
+      <div class="col-4">
+        <strong>{{$upload->title}}</strong>
+
+        <dl>
+          <dt>URI</dt>
+          <dd>{{$upload->uri}}</dd>
+
+          <dt>MIME Type</dt>
+          <dd>{{$upload->mime_type}}</dd>
+
+          <dt>Created at</dt>
+          <dd>{{$upload->created_at}}</dd>
+          
+          <dt>Last updated</dt>
+          <dd>{{$upload->updated_at}}</dd>
+
+        </dl>
+        
+        <a class="btn btn-primary" href="{{ route('uploads.edit', $upload) }}" role="button">Edit</a>
+        
+        <form method="POST" action="{{ route('uploads.destroy', $upload) }}">
+          @csrf
+          @method("DELETE")
+          <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
+      </div>
+    </div>
+    
+  </div>
+@endsection()
