5fde0bc4b1097815553d87d22148df543097a4d4
Lanang Wisnugiri <lanangwisnugiri@gmail.com>
2020-07-11 17:25:29 +0800

    issues/7: removed unnecessary locations routes from routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -30,12 +30,3 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('/events', 'EventController');
 Route::resource('/uploads', 'UploadsController');
 Route::resource('/locations', 'LocationsController');
-
-// Route::get('/locations', 'LocationsController@index')->name('home');
-// Route::post('/locations', 'LocationsController@store');
-// Route::get('/locations /create', 'LocationsController@create');
-// Route::get('/locations /{locations}', 'LocationsController@show');
-// Route::get('/locations /{locations} /edit', 'LocationsController@edit');
-// Route::put('/locations /{locations}', 'LocationsController@update');
-// Route::patch('/locations /{locations}', 'LocationsController@update');
-// Route::delete('/locations /{locations}', 'LocationsController@destroy');
