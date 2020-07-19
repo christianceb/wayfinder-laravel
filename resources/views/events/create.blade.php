acc36ffd8e971a87f990563d5fd4b274b0a26435
Lanang Wisnugiri <lanangwisnugiri@gmail.com>
2020-07-19 18:07:47 +0800

    features/lanang115/63: Changed UI on events and minor fixes on locations and users index

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,48 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Add Event</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ url('/events')}}">back</a>
-        </div>
-    </div>
-</div>
+    <div class="container">
+        <h1>Add Event</h1>
 
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
-<form action="{{route('events.store')}}" method="post">
-    @csrf
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Title:</strong>
-                <input type="text" name="title" class="form-control" placeholder="title" />
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
-        </div>
+        @endif
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
+        <form action="{{route('events.store')}}" method="post">
+            @csrf
+
             <div class="form-group">
-                <strong>Description:</strong>
-                <textarea class="form-control" name="description" placeholder="description"></textarea>
+                <label>Title</label>
+                <input type="text" name="title" class="form-control" placeholder="Enter Title here.."/>
             </div>
-        </div>
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Location:</strong>
-                <select id="eventLocation" class="form-control" name="location_id" >
+                <label>Description</label>
+                <textarea class="form-control" name="description" placeholder="Enter Description here.."></textarea>
+            </div>
+
+            <div class="form-group">
+                <label>Location</label>
+                <select id="eventLocation" class="form-control" name="location_id">
                     <option selected disabled>Choose...</option>
                     <optgroup label="Campus">
                         @foreach($locations["campus"] as $location)
@@ -61,25 +48,19 @@
                     </optgroup>
                 </select>
             </div>
-        </div>
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Start:</strong>
-                <input class="form-control" type="text" name="start" data-flatpickr-datetime />
+                <label>Start</label>
+                <input class="form-control" type="text" name="start" data-flatpickr-datetime/>
             </div>
-        </div>
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>End:</strong>
-                <input class="form-control" type="text" name="end" data-flatpickr-datetime />
+                <label>End</label>
+                <input class="form-control" type="text" name="end" data-flatpickr-datetime/>
             </div>
-        </div>
 
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <a class="btn btn-danger" href="{{ url('/events')}}">Cancel</a>
+        </form>
     </div>
-</form>
 @endsection
