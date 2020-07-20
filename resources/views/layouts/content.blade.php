fc2c54961c5d54794c2b7586d200562aa6cdf1d7
Christian Ponce <55345116+christianconcepton@users.noreply.github.com>
2020-07-20 18:14:29 +0800

    Cleanup base template and implement sticky footer

---
+++ resources/views/layouts/content.blade.php
@@ -0,0 +1,3 @@
+<main role="main" class="flex-shrink-0 py-3">
+  @yield('content')
+</main>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,10 @@
-<footer class="bg-dark">
-  <div class="container footer-container bg-dark pt-2 pb-2">
+<footer class="bg-dark mt-auto">
+  <div class="container bg-dark py-2">
     <div class="row">
       <div class="col copy">
         <div class="text-white">Wayfinder &copy; 2019 &mdash; <?= date("Y") ?></div>
       </div>
-      <div class="col contact-link text-right my-auto">
+      <div class="col contact-link text-right">
         <a href="#" class="text-white">Contact Us</a>
       </div>
     </div>
