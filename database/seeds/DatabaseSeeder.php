4630091ff7a35613f947386d1b39f0a32ccc2b4a
Lanang Wisnugiri <lanangwisnugiri@gmail.com>
2020-07-21 15:57:08 +0800

    features/lanang115-marcus/66: implemented seeder for locations and events

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class
+            UserSeeder::class,
+            LocationsSeeder::class,
+            EventSeeder::class
         ]);
     }
 }
