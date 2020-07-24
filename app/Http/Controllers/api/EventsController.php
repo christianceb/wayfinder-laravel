1de00a141f517c5ce69a2d673cf4661c7a74cd73
Christian Ponce <55345116+christianconcepton@users.noreply.github.com>
2020-07-24 23:45:57 +0800

    Improved variable names in APIs and implemented eager (lazy) loading on some important properties

--- app/Http/Controllers/api/EventsController.php
+++ app/Http/Controllers/api/EventsController.php
@@ -1,16 +1,17 @@
 <?php
 
-
 namespace App\Http\Controllers\api;
-
 
 use App\Event;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class EventsController extends Controller
 {
+    private $showProps = ['attachment', 'location'];
+
     /**
      * Display a listing of the resource.
      *
@@ -18,46 +19,57 @@ class EventsController extends Controller
      */
     public function index(Request $request)
     {
-        $events = Event::all();
+        $status = Response::HTTP_BAD_REQUEST;
+        $content = [];
+
+        $events = Event::with($this->showProps);
 
         if ($request->has('title')) {
-            $events = Event::where('title','like', "%{$request->title}%")->get();
+            $events->where('title','like', "%{$request->title}%");
         }
 
         if ($request->has('after')) {
-            $events = Event::where([
-                            ['start', '>=', $request->after],
-                            ['end', '<=', $request->after]
-                            ])
-                            ->orWhere('end', '>=', $request->after)
-                            ->orderBy('start', 'asc')->get();
+            $events->where([
+                ['start', '>=', $request->after],
+                ['end', '<=', $request->after]
+            ])
+            ->orWhere('end', '>=', $request->after)
+            ->orderBy('start', 'asc');
         }
 
         if ($request->has('before')) {
-            $events = Event::where([
-                            ['start', '<=', $request->before],
-                            ['end', '>=', $request->before]
-                            ])
-                            ->orWhere('start', '<=', $request->before)
-                            ->orderBy('start', 'desc')->get();
+            $events->where([
+                ['start', '<=', $request->before],
+                ['end', '>=', $request->before]
+            ])
+            ->orWhere('start', '<=', $request->before)
+            ->orderBy('start', 'desc');
         }
 
-        return response([
-            'locations' => EventResource::collection($events)->paginate(5),
-            'message' => 'Retrieved Successfully',
-        ], 200);
+        $events = $events->get();
+        $status = Response::HTTP_NOT_FOUND;
+
+        if ($events->isNotEmpty()) {
+            $content['result'] = EventResource::collection($events)->paginate(5);
+            $status = Response::HTTP_OK;
+        }
+
+        $content['message'] = Response::$statusTexts[$status];
+
+        return response($content, $status);
     }
+
     /**
      * Display the specified resource.
      *
-     * @param  \App\Location $location
+     * @param  \App\Event $event
      * @return \Illuminate\Http\Response
      */
     public function show(Event $event)
     {
         return response([
-            'location' => new EventResource($event),
-            'message' => 'Retrieved Successfully',
-        ], 200);
+            'result' => new EventResource($event->load($this->showProps)),
+            'message' => Response::$statusTexts[Response::HTTP_OK],
+        ], Response::HTTP_OK);
     }
 }
